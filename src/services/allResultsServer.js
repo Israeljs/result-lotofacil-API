32b414478a69d7ff2ab5824d28d89d606accb000
israel jeronimo <israel.nodejs@gmail.com>
2026-07-24 20:31:02 -0300

    feat: cria o arquivo allResultsController.js

---
+++ src/services/allResultsServer.js
@@ -0,0 +1,85 @@
+import fs from 'node:fs/promises';
+import oldResults from '../database/results.js';
+// const oldResults = oldestResults
+console.log(oldResults[0].numero)
+// Retorna o último resultado da lotofácil
+const lastResultFetcher = async () => {
+  const response = await fetch(
+    'https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/'
+  )
+  return response.json()
+}
+console.log(await lastResultFetcher())
+// Retorna qualquer resultados da lotofácil
+const anyResultFetcher = async (draw) => {
+  const response = await fetch(
+    `https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/${draw}`
+  )
+  return response.json()
+}
+// console.log(await anyResultFetcher(3733));
+// Retorna todos os resultados da lotofácil
+const lotofacilResults = async () => {
+  const lastResult = await lastResultFetcher()
+  const lastResultNumber = Number(lastResult.numero)
+  if (!oldResults.length) {
+    throw new Error('Nenhum resultado encontrado no arquivo.')
+  }
+  const oldResultNumber = Number(oldResults[0].numero)
+  for (let i = oldResultNumber + 1; i <= lastResultNumber; i++) {
+    oldResults.unshift(await anyResultFetcher(i))
+  }
+  // console.log(oldResults)
+  return oldResults
+
+}
+// console.log(await lotofacilResults());
+console.log('Antes de escrever');
+
+const writeAllResults = async () => {
+  try {
+    const data = await lotofacilResults()
+    console.log(data);
+    await fs.writeFile(
+      'src/database/results.json',
+      `export default ${JSON.stringify(data, null, 2)}`, 
+      {
+        encoding: 'utf-8',
+        flag: 'w'
+      }
+    )
+  } catch (err) {
+
+    console.error(err)
+  }
+}
+writeAllResults()
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//   try {
+//     const data = await lotofacilResults()
+//     console.log(data);
+//     await fs.writeFile(
+//       'src/database/results.json',
+//       `export default ${JSON.stringify(data, null, 2)}`, 
+//       {
+//         encoding: 'utf-8',
+//         flag: 'w'
+//       }
+//     )
+//   } catch (err) {
+
+//     console.error(err)
+//   }
+// })()
